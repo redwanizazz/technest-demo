ff5a08e19adf6471568a8d392b9aad5d9f1d589e
Redwan <redwanizaz10@gmail.com>
2025-02-09 21:22:53 +0600

    TillMongoDBSetup

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import axios from 'axios';
 
 const ProductScreen = () => {
-    const { id: productId } = useParams()
-    const product = products.find(p => p._id === productId)
+    const [product, setProduct] = useState({});
+
+    const { id: productId } = useParams();
+
+    useEffect(() => {
+
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
 
     return <>
         <Link className='btn btn-light my-3' to='/'>
